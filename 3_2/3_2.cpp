e285e7614fa5bc1d868f16be9101ed51973db5fc
savko <savko@NOTE>
2024-03-19 22:28:17 +0300

    Добавьте файлы проекта.

---
+++ 3_2/3_2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <fstream>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    const int N = 12;
+    int counter = 0;
+    fstream ffile("ffile.bin", ios::binary | ios::in | ios::out);
+    fstream gfile("gfile.bin", ios::binary | ios::in | ios::out);
+    int array[N] {0};
+    if (!ffile/* || !gfile*/)
+    {
+        cout << "Error: file cant be opened" << endl;
+        return 1;
+    }
+    
+    for (int i = 1; i <= N; i++)
+    {
+        int v = rand() % 100;
+        ffile.write(reinterpret_cast <char*> (&v), sizeof(int));
+    }
+    int ads = 0;
+    while (!ffile.eof())
+    {
+        cout<<ads<<" ";
+        ffile.read(reinterpret_cast<char*> (&ads),sizeof(int));
+        
+    }
+
+    gfile.flush();
+}
---
+++ OOP/OOP.cpp
@@ -0,0 +1,85 @@
+﻿#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int counter = 0;
+
+void Read(fstream& ffile, int* massive);
+void SortObmen(int* massive, short counter);
+void Replace(int* massive, int counter);
+void Record(ofstream& gfile, int* massive, int counter);
+	
+int main()
+{
+
+
+	fstream ffile("file.txt", ios::in | ios::out);
+	ofstream gfile("g.txt");
+	int c = 0;
+	if (!ffile || !gfile)
+	{
+		cout << "error";
+	}
+	while (!ffile.eof())
+	{
+		ffile >> c;
+		counter++;
+		cout << c << " ";
+	}
+	int* massive = new int [counter] { 0 };
+	Read(ffile, massive);
+	SortObmen(massive, counter);
+	Replace(massive, counter);
+	Record(gfile, massive, counter);
+
+	gfile.close();
+	ffile.close();
+	return 0;
+}
+
+void Read(fstream& ffile, int* massive)
+{
+	int l = 0;
+	ffile.seekg(0);
+	while (!ffile.fail())
+	{
+		ffile >> massive[l];
+		l++;
+	}	 
+}
+
+
+void SortObmen(int* massive, short counter)
+{
+	int min = massive[0];
+	for (int i = 0; i < counter - 1; i++)
+	{
+		for (int j = counter - 1; j > i; j--)
+		{
+			if (massive[j - 1] > massive[j])
+			{
+				min = massive[j - 1];
+				massive[j - 1] = massive[j];
+				massive[j] = min;
+			}
+		}
+	}
+}
+
+void Replace(int* massive, int counter)
+{
+	for (int i = 0; i < counter/2; i += 2)
+	{
+		swap(massive[i], massive[counter - i-1]);
+	}
+}
+
+void Record(ofstream& gfile, int* massive, int counter)
+{
+
+	for (int i = 0; i < counter; i++)
+	{
+		gfile << massive[i]<<" ";
+	}
+}
